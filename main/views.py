bec29950c5c8dd0ed7f39b9f9bdd674d6e9bd016
kofe <petrovykh03@gmail.com>
2022-05-08 21:04:12 +0300

    TaskManager

---
+++ main/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+
+def index(request):
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'main/index.html', {'title': 'Главная страница сайта', 'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма некорректна'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
+
+
+def delete(request, id):
+    try:
+        tasks = Task.objects.get(id=id)
+        tasks.delete()
+        return redirect('home')
+    except:
+        return redirect('home')
